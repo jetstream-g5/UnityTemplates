3af4391a4d4d862adc8967a022847a9ac773830f
Lorenzo <18927@ma-web.nl>
2015-09-01 12:57:18 +0200

    hi

---
+++ EditorSkip.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+[ExecuteInEditMode]
+public class EditorSkip : MonoBehaviour {
+
+
+	#if UNITY_EDITOR
+	void Start () {
+		Application.LoadLevel ("Level");
+	}
+	#endif
+}
---
+++ Sceneswitch.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sceneswitch : MonoBehaviour {
+
+	IEnumerator Start () {
+		yield return new WaitForSeconds (5f);
+		Application.LoadLevel ("Menu");
+	}
+}
